d1adc97b4999b54bf213190c974a7fa79b07f4d1
Jesse De Jong <jdoceau@gmail.com>
2022-04-11 11:12:07 +1000

    working hover for about

--- script.js
+++
@@ -1,4 +0,0 @@
-// DOM Targeting
-const aboutMe = document.getElementsByClassName('about');
-
-{/* <p  class="about__script">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestiae, error? Culpa id, deleniti consectetur omnis debitis voluptatem, et fugiat recusandae optio quasi labore vel quod rem ut doloremque cupiditate consequuntur?</p> */}
